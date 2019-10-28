19abddaadbdcf047634f23ff1e73be2f5ef72936
Andrew Tang <atreeleaf@gmail.com>
2019-10-27 23:09:06 -0700

    adding notes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const mapResolvers = require("./mapResolvers")
-// const { trackError } = require("../bin/db/fileController.js")
 
 
+/**
+ * 
+ * @param {*} resolvers - this is the resolvers object with your GraphQL Schema
+ * @param {*} accessID  - accessID can be retrieved from our website
+ */
 const enableMonitoring = (resolvers,accessID) => {
 const injectedResolvers = mapResolvers(resolvers,accessID)
 return injectedResolvers
